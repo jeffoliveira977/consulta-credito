18db4ad1d7091043811ce4d46000bc872acc3f03
Jeff <jeffoliveira977@gmail.com>
2025-05-03 14:55:19 -0300

    Projeto inicial: consulta de crédito

---
+++ credito-api/src/main/java/com/consulta/credito/api/ConsultaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.consulta.credito.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCreditoApplication.class, args);
+	}
+
+}
---
+++ credito-api/src/test/java/com/consulta/credito/api/ConsultaCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulta.credito.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
